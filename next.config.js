1fb1a5c7e29a35214804e966eb8cf3a6b928fc49
Profe Matias Prestes <profematiasprestes@gmail.com>
2025-04-03 21:55:40 -0300

    prueba

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Adjust this to match your GitHub repository name
+  basePath: '/03-Tienda',
+  assetPrefix: '/03-Tienda/',
+  trailingSlash: true,
 }
+
+module.exports = nextConfig
