0f15383dd334e5ccdffe3fcf30852b97caec5c64
Profe Matias Prestes <profematiasprestes@gmail.com>
2025-04-06 21:19:33 -0300

    3

--- dist/next.config.dev.js
+++ dist/next.config.dev.js
@@ -1,9 +1,11 @@
+"use strict";
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   // Add this if you're using Next.js
-  webpack: (config) => {
+  webpack: function webpack(config) {
     config.resolve.extensions.push('.ts', '.tsx');
     return config;
   }
-}
+};
